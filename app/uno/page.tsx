0b55bb8daaa022d7bd6979658edd0bd2e12a9d61
Francisco Linares <edolinares@gmail.com>
2024-04-14 21:53:19 -0700

    #

--- app/uno/page.tsx
+++ app/uno/page.tsx
@@ -2,14 +2,14 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
-import { Megaphone, Search, SquareCheckBig, ToggleLeft } from "lucide-react";
+import { Home, Megaphone, Pickaxe, Search, Server, Spade, SquareCheckBig, Star, ToggleLeft, TrafficCone, TreePalm } from "lucide-react";
 
 const PageUno = () => {
     return (
         <div className="h-screen w-screen bg-[#f4f4f7] ">
             <div className="flex flex-row justify-between px-8 py-2 ">
                 <div >
-                    <h1 className="h-full content-center">MyLogo</h1>
+                    <h1 className="h-full content-center text-3xl">MyLogo</h1>
                 </div>
                 <div className="grow px-8">
                     <Input className="rounded-full px-8" placeholder="Search..." />
@@ -92,6 +92,41 @@ const PageUno = () => {
                             </Card>
                         </div>
                 </div>
+
+
+            <div className="absolute bg-white inset-y-0 left-20 h-96 rounded-full top-1/2  transform -translate-x-1/2 -translate-y-1/2 p-1 border">
+                <div className="flex flex-col space-y-4 place-items-center p-1">
+                    <div className="rounded-full bg-[#0b3fff] p-1.5">
+                        <Home color="white" className="w-6 h-6" />
+                    </div>
+                    <div className="rounded-full hover:bg-[#0b3fff]/50 p-1.5">
+                        <TreePalm  className="w-6 h-6 stroke-[#35353b] hover:stroke-white " />
+                    </div>
+                    <div className="rounded-full hover:bg-[#0b3fff]/50 p-1.5">
+                        <TrafficCone   className="w-6 h-6 stroke-[#35353b] hover:stroke-white " />
+                    </div>
+                    <div className="rounded-full hover:bg-[#0b3fff]/50 p-1.5">
+                        <Star  className="w-6 h-6 stroke-[#35353b] hover:stroke-white "  />
+                    </div>
+                    <div className="rounded-full hover:bg-[#0b3fff]/50 p-1.5">
+                        <Spade className="w-6 h-6 stroke-[#35353b] hover:stroke-white " />
+                    </div>
+                    <div className="rounded-full hover:bg-[#0b3fff]/50 p-1.5">
+                        <Server   className="w-6 h-6 stroke-[#35353b] hover:stroke-white " />
+                    </div>
+                    <div className="rounded-full hover:bg-[#0b3fff]/50 p-1.5">
+                        <Pickaxe   className="w-6 h-6 stroke-[#35353b] hover:stroke-white " />
+                    </div>
+                </div>
+
+            </div>
+
+
+
+
+
+
+
             </div>
 
     );
